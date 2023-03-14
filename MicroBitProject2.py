8db5092f608550cbaa083fdd888c30ea9d3b3afa
Anantjit <anantjitchander@gmail.com>
2023-03-15 00:07:17 +0530

    Update MicroBitProject2.py

--- MicroBitProject2.py
+++ MicroBitProject2.py
@@ -1,18 +1,22 @@
 from microbit import *
 import music
 import radio
+import log
 radio.config(group=4)
+radio.on()
 
 while True:
-        
+    
     message=radio.receive()
     if message == "Mi":
+        log.add({'Scenario':message})
         for i in range(1,5):
             music.play(['d', 'a'])
         music.stop()
         display.scroll('Noise. Noise.')
     elif message == "Ma":
+        log.add({'Scenario':message})
         for i in range(1,5):
-            music.play(['d', 'a'])
+            music.play(['c', 'g'])
         music.stop()
         display.scroll('Door. Door')
